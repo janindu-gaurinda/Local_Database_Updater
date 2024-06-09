5988e484f0ddd1f13ff1b7ac43510ef1a959a64e
Janindu Gaurinda <154628076+janindu-gaurinda@users.noreply.github.com>
2024-06-09 22:54:29 +0530

    github.sql

--- github.sql
+++ github.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2024 at 05:42 PM
+-- Generation Time: Jun 09, 2024 at 06:47 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -29,7 +29,6 @@ USE `github`;
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
@@ -37,8 +36,32 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- RELATIONSHIPS FOR TABLE `admin`:
+-- Dumping data for table `admin`
 --
+
+INSERT INTO `admin` (`admin_id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$CtWuMRsyZ/cqIV26a3/NKOx0abWxPzHZL3epX02EmjsMDWbDPjH0e'),
+(2, 'admin', '$2y$10$VT.5RuY56PB3njEvcxotwuO3qOibU0CpumKQFhwIt0zPa7zgR4pvG'),
+(3, 'aaa', '$2y$10$.snXRxUja88F8vC55vLa9erEDoF0LTHWmCcz7nJIUbwN7AV.2s976'),
+(4, 'admin', '$2y$10$i1HriKfzDcexFerlNOfqgu5/6B254fChU/jmXQCT.8XdbuvsCl7eS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `item_name`) VALUES
+(1, 'Test 1');
 
 --
 -- Indexes for dumped tables
@@ -51,6 +74,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -58,7 +87,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
